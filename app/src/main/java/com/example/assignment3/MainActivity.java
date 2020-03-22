4475c495e21cc977c3d7594e26932a98ec5266c3
BirdsArentReal <rsyj04@gmail.com>
2020-03-22 19:54:23 +0800

    RE

--- app/src/main/java/com/example/assignment3/MainActivity.java
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -13,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+
+        int checked = sharedPref.getInt("checked", 0);
+        if (checked == 1){
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+        else{
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt("Checked", 1);
+            editor.commit();
+        }
     }
 
     public void onAgreeClicked(View view){
